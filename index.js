eec96273d157918e796df9159c4fc7a90f881590
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-14 12:10:09 +0530

    created a server running at 5001 port

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Server entry point // 
+
+// Nescessary imports
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+// importing all routers
+
+// importing database config code and rendering for config folder
+import './config/config.js'
+
+// constructed a server
+const SERVER = express();
+
+// parsing the json, cors policy url encoding and json formatting
+SERVER.use(bodyParser.json());
+SERVER.use(cors());
+SERVER.use(express.urlencoded({ extended: true }));
+SERVER.use(express.json());
+
+//MIDDLEWARES
+
+// SERVER.use("login",authe)
+
+// defining port for backend rest server
+const PORT = 5001;
+
+// run the server at PORT
+SERVER.listen(PORT, () =>
+  console.log(`server running at: http://localhost:${PORT} `)
+);
