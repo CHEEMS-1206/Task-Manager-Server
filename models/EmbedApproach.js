d2d69a3f320d305ee0743fb17a2a0909b159b664
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-23 20:20:07 +0530

    defined a new model having embedded approach such that for every user we have distinct tasks

---
+++ models/EmbedApproach.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  taskName: {
+    type: String,
+    required: true,
+  },
+  taskDescription: {
+    type: String,
+    required: true,
+  },
+  taskId: {
+    type: String,
+    unique: true,
+  },
+  taskCreatedAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  taskDeadline: {
+    type: Date,
+    required: true,
+  },
+  taskStatus: {
+    type: String,
+    enum: ["Completed", "Pending", "Missed"],
+    default: "Pending",
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address.`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length >= 8 && v.length <= 12;
+      },
+      message: (props) =>
+        `${props.value} is not a valid password. Password must be 8 to 12 characters.`,
+    },
+  },
+  userName: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9]{5,}$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid username. Username must be alphanumeric and at least 5 characters long.`,
+    },
+  },
+  tasks: [taskSchema], // Embedding tasks within the user document
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
